5afaf27deee8b3f5cee8d37e8ff33716fa00a9a1
CliffordDelaCruz <123575331+CliffordDelaCruz@users.noreply.github.com>
2023-12-23 13:18:24 +0800

    add new python code about using numpy for binomial distribution

---
+++ binomial_distsample.py
@@ -0,0 +1,7 @@
+#binomial_dist.py
+import numpy as np
+# Note that the following code:
+# Single experimet is done 100 times
+# Probability is 80%
+# Size is 500
+print(np.random.binomial(n=100, p=0.8, size = 500))
